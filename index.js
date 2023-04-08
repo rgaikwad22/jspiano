536b32adb939993f6412c89de9b154c8840430ae
rgaikwad22 <rushikeshgaikwad238@gmail.com>
2023-04-08 15:39:57 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const keys = document.querySelectorAll(".key");
+const note = document.querySelector(".nowplaying");
+const hints = document.querySelectorAll(".hints");
+
+function playNote(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`),
+  key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  
+  if (!key) return;
+  
+  const keyNote = key.getAttribute("data-note");
+  
+  key.classList.add("playing");
+  note.innerHTML = keyNote;
+  audio.currentTime = 0;
+  audio.play();
+}
+
+
+function removeTransition(e) {
+  if (e.propertyName !== "transform")
+  return;
+  this.classList.remove("playing");
+}
+
+
+function hintsOn(e, index) {
+  e.setAttribute("style", "transition-delay:" + index * 50 + "ms");
+}
+
+hints.forEach(hintsOn);
+
+keys.forEach(key => key.addEventListener("transitionend", removeTransition));
+
+window.addEventListener("keydown", playNote);
